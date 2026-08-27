3fdd39675e9735f7f007b5cd607da54196cc5629
victor15-design <chiemico678@gmail.com>
2026-08-27 08:56:32 +0100

    revamp4 commit

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,20 +7,23 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'phone_number')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'role')
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
+    phone_number = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = User
         fields = ('email', 'password', 'first_name', 'last_name', 'phone_number')
 
     def create(self, validated_data):
+        email = validated_data['email']
         user = User.objects.create_user(
-            username=validated_data['email'],
-            email=validated_data['email'],
+            username=email,
+            email=email,
             password=validated_data['password'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
@@ -29,6 +32,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = User.USERNAME_FIELD
+
     def validate(self, attrs):
         data = super().validate(attrs)
         data['user'] = UserSerializer(self.user).data
